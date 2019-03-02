ac1d18b06928c97d1455e1897761c09f855ae732
Melisa Johnson <melisa@asteri-networks.com>
2019-03-02 15:25:53 -0600

    Fixes Audio Manager

--- Assets/Scripts/Manager/AudioManager.cs
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -31,40 +31,58 @@ public class AudioManager : MonoBehaviour
     public AudioSource backGroudSource;
     public AudioSource shotsSource;
 
+    //public bool shouldLoop = false;
+    public static AudioManager Instance;
     //public AudioClip backGroundMusic;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+    }
     private void Start()
     {
         backGroudSource = backGroudSource.GetComponent<AudioSource>();
         shotsSource = shotsSource.GetComponent<AudioSource>();
     }
 
-    public void SoundsEventTrigger(SoundEvents soundEvents)
+    public void SoundsEventTrigger(SoundEvents soundEvents, bool shouldLoop)
     {
         switch (soundEvents)
         {
             case SoundEvents.Pistal:
-                PistalClips();
+                PistalClips(shouldLoop);
                 break;
             case SoundEvents.Rifle:
-                RifleClips();
+                RifleClips(shouldLoop);
                 break;
             case SoundEvents.Railgun:
-                RailGunClips();
+                RailGunClips(shouldLoop);
                 break;
             case SoundEvents.ChainGun:
-                ChainGunClips();
+                ChainGunClips(shouldLoop);
                 break;
             case SoundEvents.BackGroundMusic:
+                BackGroundMusic(shouldLoop);
+                break;
+            case SoundEvents.Ambient:
+                AmbientClip(shouldLoop);
                 break;
 
         }
     }
 
-    public void PistalClips()
+    public void PistalClips(bool loop)
     {
         int randomInt = Random.Range(0, pistalClips.Count-1);
         shotsSource.clip = pistalClips[randomInt].audio;
-        if (pistalClips[randomInt].shouldLoop)
+        if (loop)
         {
             shotsSource.loop = true;
         }
@@ -75,11 +93,11 @@ public class AudioManager : MonoBehaviour
         shotsSource.Play();
     }
 
-    private void RifleClips()
+    private void RifleClips(bool loop)
     {
         int randomInt = Random.Range(0, rifleClips.Count - 1);
         shotsSource.clip = rifleClips[randomInt].audio;
-        if (rifleClips[randomInt].shouldLoop)
+        if (loop)
         {
             shotsSource.loop = true;
         }
@@ -89,11 +107,11 @@ public class AudioManager : MonoBehaviour
         }
         shotsSource.Play();
     }
-    private void RailGunClips()
+    private void RailGunClips(bool loop)
     {
         int randomInt = Random.Range(0, railGunClips.Count - 1);
         shotsSource.clip = railGunClips[randomInt].audio;
-        if (railGunClips[randomInt].shouldLoop)
+        if (loop)
         {
             shotsSource.loop = true;
         }
@@ -103,11 +121,11 @@ public class AudioManager : MonoBehaviour
         }
         shotsSource.Play();
     }
-    private void ChainGunClips()
+    private void ChainGunClips(bool loop)
     {
         int randomInt = Random.Range(0, chainGunClips.Count - 1);
         shotsSource.clip = chainGunClips[randomInt].audio;
-        if (chainGunClips[randomInt].shouldLoop)
+        if (loop)
         {
             shotsSource.loop = true;
         }
@@ -118,16 +136,32 @@ public class AudioManager : MonoBehaviour
         shotsSource.Play();
     }
 
-    private void BackGroundMusic()
+    private void BackGroundMusic(bool loop)
     {
             int randomInt = Random.Range(0, backGroundClips.Count);
             backGroudSource.clip = backGroundClips[randomInt].audio;
+        if (loop)
+        {
+            backGroudSource.loop = true;
+        }
+        else
+        {
+            backGroudSource.loop = false;
+        }
         backGroudSource.Play();
     }
 
-    private void AmbientClip()
+    private void AmbientClip(bool loop)
     {
         backGroudSource.clip = ambientClips.audio;
+        if (loop)
+        {
+            backGroudSource.loop = true;
+        }
+        else
+        {
+            backGroudSource.loop = false;
+        }
         backGroudSource.Play();
     }
 }
