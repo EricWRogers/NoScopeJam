4a4cb5811136df96e93b58f0241bfc637234d1f8
Azee <aziztitu1996@gmail.com>
2019-03-02 14:07:46 -0600

    Snappy Wall Jumps Babyy!!!

--- Assets/Scripts/Player/CustomFirstPersonController.cs
+++ Assets/Scripts/Player/CustomFirstPersonController.cs
@@ -66,17 +66,17 @@ public class CustomFirstPersonController : MonoBehaviour
     private bool m_Jumping;
     private AudioSource m_AudioSource;
 
-    private bool _isWallRunning = false;
+    [SerializeField] [ReadOnly] private bool _isWallRunning = false;
     private Vector3 _wallNormal = Vector3.zero;
     private Vector3 _wallRunDir = Vector3.zero;
     private float _wallRunChargeLeft = 100f;
 
-    private bool _isUsingThrusters = false;
+    [SerializeField] [ReadOnly] private bool _isUsingThrusters = false;
     private float _thrusterChargeLeft = 100f;
     private bool _requiresThrusterJumpStart = false;
     private Vector3 _extraThrusterForce = Vector3.zero;
 
-    private bool _isSliding = false;
+    [SerializeField] [ReadOnly] private bool _isSliding = false;
     private float _slidingChargeLeft = 100f;
 
 
@@ -225,6 +225,11 @@ public class CustomFirstPersonController : MonoBehaviour
             GroundMove(speed);
         }
 
+        if (_playerInputController.GetPlayerInput().JumpStart)
+        {
+            Debug.Log("Jumping");
+        }
+
         if (wasWallRunning || !_isWallRunning)
         {
             UseThrusters();
@@ -284,10 +289,34 @@ public class CustomFirstPersonController : MonoBehaviour
         {
             if (!_isWallRunning && (playerInput.JumpStart || (!m_CharacterController.isGrounded && playerInput.Jump)))
             {
+                int playerLayerMask = LayerMask.NameToLayer("Player");
+
+                Vector3 raycastDir1 = moveWorldDir;
+                Vector3 raycastDir2 = Vector3.zero;
+
+                if (!m_CharacterController.isGrounded)
+                {
+                    if (_extraThrusterForce.magnitude > 0)
+                    {
+                        Vector3 localDir = transform.InverseTransformDirection(_extraThrusterForce.normalized);
+                        localDir.y = 0;
+                        localDir.z = move.z;
+                        raycastDir1 = transform.TransformDirection(localDir);
+                    }
+                    else
+                    {
+                        Vector3 localDir = new Vector3(-1, 0, move.z);
+                        raycastDir1 = transform.TransformDirection(localDir);
+                        localDir = new Vector3(1, 0, move.z);
+                        raycastDir2 = transform.TransformDirection(localDir);
+                    }
+                }
+
                 RaycastHit hit;
-                if (Physics.Raycast(transform.position, moveWorldDir, out hit,
-                    _wallRunMaxDistance,
-                    LayerMask.NameToLayer("Player")))
+                if (Physics.Raycast(transform.position, raycastDir1, out hit,
+                        _wallRunMaxDistance, playerLayerMask)
+                    || Physics.Raycast(transform.position, raycastDir2, out hit,
+                        _wallRunMaxDistance, playerLayerMask))
                 {
                     _isWallRunning = true;
 
@@ -361,16 +390,18 @@ public class CustomFirstPersonController : MonoBehaviour
 
         if (_thrusterChargeLeft > 0)
         {
-//            Debug.Log("Using Thruster...");
+            Debug.Log("Using Thruster...");
             if (_requiresThrusterJumpStart && playerInput.JumpStart)
             {
                 _requiresThrusterJumpStart = false;
+
+                Debug.Log("Removes Jump Start requirement");
             }
 
             if (_isWallRunning && playerInput.JumpStart)
             {
-                _extraThrusterForce = _wallNormal * _thrusterWallImpluse;
-                _extraThrusterForce += _wallRunDir * 100;
+                _extraThrusterForce = _wallNormal.normalized * _thrusterWallImpluse;
+                _extraThrusterForce += _wallRunDir.normalized * 100;
 
                 _isWallRunning = false;
                 _wallRunChargeLeft = 100;
