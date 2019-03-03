e5788eec82a14d9e7f6ad6f74846cb803d462e53
Azee <aziztitu1996@gmail.com>
2019-03-02 20:18:26 -0600

    Implements Pickups. Adds JSON .Net

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,12 +1,15 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class PlayerStats : MonoBehaviour
 {
-    public float health;
-    public float ammo;
+    public float health { get; private set; }
+    private Dictionary<GunType.Ammo, int> ammo = new Dictionary<GunType.Ammo, int>();
 
     public static PlayerStats Instance = null;
-    
+
     public void Awake()
     {
         if (Instance == null)
@@ -17,5 +20,29 @@ public class PlayerStats : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+    }
+
+    public void AddAmmoCount(GunType.Ammo ammoType, int ammoCount)
+    {
+        if (!ammo.ContainsKey(ammoType))
+        {
+            ammo[ammoType] = 0;
+        }
+
+        ammo[ammoType] += ammoCount;
+    }
+
+    public int GetAmmoCount(GunType.Ammo ammoType)
+    {
+        int ammoCount = 0;
+        ammo.TryGetValue(ammoType, out ammoCount);
+
+        return ammoCount;
+    }
+
+    public void UpdateHealth(float updateAmount)
+    {
+        health += updateAmount;
+        health = Mathf.Clamp(health, 0, 100);
     }
 }
